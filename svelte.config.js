2fa27feb92d783cb8f11d96eeb235ffed7ce3964
Daniel Maricic <daniel@woss.io>
2022-03-28 15:09:58 +0200

    add husky and lint the main files
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- svelte.config.js
+++ svelte.config.js
@@ -7,24 +7,9 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
-	// kit: {
-	// 	adapter: adapter(),
-	// }
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		}),
-		routes: (filepath) => !/(?:(?:^_|\/_)|(?:^\.|\/\.)(?!well-known))/.test(filepath),
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
-	}
+		adapter: adapter(),
+	},
 };
 
 export default config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,60 +1,55 @@
 module.exports = {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    './components/**/*.{html,js,svelte,ts}',
-  ],
-  darkMode: 'class',
-  theme: {
-    theme: {
-      screens: {
-        xs: '320px',
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
-      colors: {
-        'slate-navy': '#054F77',
-        'mid-navy': '#093954',
-        'almost-navy': '#052233',
-        'light-green': '#8CFF00',
-        'text-colors':'#F4FBFF',
+	content: ['./src/**/*.{html,js,svelte,ts}', './components/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	theme: {
+		theme: {
+			screens: {
+				xs: '320px',
+				sm: '480px',
+				md: '768px',
+				lg: '976px',
+				xl: '1440px',
+			},
+			colors: {
+				'slate-navy': '#054F77',
+				'mid-navy': '#093954',
+				'almost-navy': '#052233',
+				'light-green': '#8CFF00',
+				'text-colors': '#F4FBFF',
 
-        
-
-        'blue': '#1fb6ff' ,
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-      },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      extend: {
-        spacing: {
-          '1': '8px',
-          '2': '12px',
-          '3': '16px',
-          '4': '24px',
-          '5': '32px',
-          '6': '48px',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
-    }
-  },
-  // plugins: [
-  //   require('@tailwindcss/typography'),
-  //   require('@tailwindcss/forms'),
-  //   require('@tailwindcss/line-clamp'),
-  //   require('@tailwindcss/aspect-ratio'),
-  // ],
-}
+				blue: '#1fb6ff',
+				purple: '#7e5bef',
+				pink: '#ff49db',
+				orange: '#ff7849',
+				green: '#13ce66',
+				yellow: '#ffc82c',
+				'gray-dark': '#273444',
+				gray: '#8492a6',
+				'gray-light': '#d3dce6',
+			},
+			fontFamily: {
+				sans: ['Graphik', 'sans-serif'],
+				serif: ['Merriweather', 'serif'],
+			},
+			extend: {
+				spacing: {
+					1: '8px',
+					2: '12px',
+					3: '16px',
+					4: '24px',
+					5: '32px',
+					6: '48px',
+				},
+				borderRadius: {
+					'4xl': '2rem',
+				},
+			},
+		},
+	},
+	// plugins: [
+	//   require('@tailwindcss/typography'),
+	//   require('@tailwindcss/forms'),
+	//   require('@tailwindcss/line-clamp'),
+	//   require('@tailwindcss/aspect-ratio'),
+	// ],
+};
