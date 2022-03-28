08b646ab933f8b9f27585199a3f4722d3b317b6f
Leonardo Monay <leonardomonay@gmail.com>
2022-03-28 13:39:58 -0300

    set gpg key to sign commits

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
 <main class="flex justify-center">
 	<div class="flex flex-col items-center w-full">
 		<h1 class="text-3xl font-bold underline p-5 bg-lightgreen">Welcome!</h1>
+		<h3 class="text-3xl font-bold underline p-5 bg-lightgreen">To anagolay website</h3>
 		<Button text="Switch theme" extraClassNames={['bg-orange']} />
 	</div>
 </main>
