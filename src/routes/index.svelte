8654244fb786ca68733d3c72c279cf17a250853b
Daniel Maricic <daniel@woss.io>
2022-03-28 16:18:37 +0200

    add base button component
    Signed-off-by: Daniel Maricic <daniel@woss.io>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
+	import Button from '../components/Button.svelte';
 </script>
 
 <main class="flex justify-center">
 	<div class="flex flex-col items-center w-full">
 		<h1 class="text-3xl font-bold underline p-5 bg-lightgreen">Welcome!</h1>
 
-		<button class=" text-gray-500 px-2 py-1 m-2 rounded-full" disabled> Switch theme </button>
+		<!-- <button class="bg-orange"> </button>\ -->
+		<Button text="Switch theme" extraClassNames={['bg-orange']} />
 		<p class="text-xs w-2/4 text-center">Now you can switch theme using the upper right button</p>
 	</div>
 </main>
 
-<style lang="postcss" global>
+<!-- <style lang="postcss" global>
 	@tailwind base;
 	@tailwind components;
 	@tailwind utilities;
-</style>
+</style> -->
