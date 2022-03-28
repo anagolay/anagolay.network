4868fccaaefdb81957639eb22c591e9838ab5ba0
leomonay <leonardomonay@gmail.com>
2022-03-28 13:13:35 -0300

    Delete all theme changing related code

--- src/components/navBar.svelte
+++ src/components/navBar.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	export let theme: string;
-	export let switchTheme: (event: Event) => void;
 
 	let toggleMenu = false;
 	function toggleSideBar() {
@@ -15,7 +13,7 @@
 	];
 </script>
 
-<nav class="flex items-center justify-between bg-main w-full text-sm py-5">
+<nav class="flex items-center justify-between bg-main w-full text-sm py-3">
 	<button class="md:hidden p-1 mx-2 top-0 left-0 bg-secondary rounded-md" on:click={toggleSideBar}>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
@@ -29,11 +27,7 @@
 		</svg>
 	</button>
 	<div class="flex justify-center items-center h-10 w-3/4 md:w-2/6">
-		<img
-			class="object-contain h-full"
-			src={theme === 'dark' ? 'horizontal_white1.svg' : 'horizontal_blue.svg'}
-			alt="Anagolay logo"
-		/>
+		<img class="object-contain h-full" src="horizontal_white1.svg" alt="Anagolay logo" />
 	</div>
 	<div class="flex">
 		<div
@@ -60,43 +54,6 @@
 				FAQ
 			</button>
 		</div>
-		<button
-			class="flex p-1 mx-2 bg-secondary rounded-md"
-			title={`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`}
-			on:click={(event) => switchTheme(event)}
-		>
-			{#if theme === 'dark'}
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6 text-yellow-300"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					stroke-width="2"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-					/>
-				</svg>
-			{:else}
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					stroke-width="2"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-					/>
-				</svg>
-			{/if}
-		</button>
 	</div>
 </nav>
 
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,53 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import NavBar from '../components/navBar.svelte';
-
-	// import { browser } from '$app/env';
-	// import { writable } from 'svelte/store';
-
-	// const defaultValue = 'summer';
-	// const initialValue = browser ? window.localStorage.getItem('theme') ?? defaultValue : defaultValue;
-
-	// export const theme = writable<string>('dark');
-
-	// theme.subscribe((value) => {
-	// if (browser) {
-	//     window.localStorage.setItem('theme', value);
-	// }
-	// });
-
-	// export { theme as default };
-
-	let theme = 'dark';
-	/**
-	 * Switch the dark light theme based on the button click event, store the theme to the localStorage @TODO
-	 * @param e
-	 */
-	function switchTheme(e: Event): void {
-		e.preventDefault();
-		const styles = document.documentElement.style;
-		const colors =
-			theme === 'dark'
-				? {
-						'--main': '#D5DBDB',
-						'--secondary': '#EAEDED',
-						'--terciary': '#FBFCFC',
-						'--primary': '#1E8449',
-						'--text': '#054F77',
-				  }
-				: {
-						'--main': '#052233',
-						'--secondary': '#093954',
-						'--terciary': '#054F77',
-						'--primary': '#8CFF00',
-						'--text': '#F4FBFF',
-				  };
-		theme = theme === 'dark' ? 'light' : 'dark';
-		for (let key of Object.keys(colors)) {
-			styles.setProperty(key, colors[key]);
-		}
-	}
 </script>
 
-<NavBar {theme} {switchTheme} />
+<NavBar />
 <slot />
