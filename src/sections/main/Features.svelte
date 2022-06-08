ce59c6641b291c940e7fdbffc348c15bd697a06a
Leonardo Monay <leonardomonay@gmail.com>
2022-06-08 09:17:25 -0300

    add space between sections in main mobile

--- src/sections/main/Features.svelte
+++ src/sections/main/Features.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <Section id="main_features">
-	<div class="text-center mb-20 md:mb-40">
+	<div class="text-center mb-40">
 		<h2 class="mb-12 text-neonGreen-400 text-2xl md:text-4xl ">
 			<p class="md:mb-4">Anagolay is a</p>
 			<p>different kind of beast</p>
