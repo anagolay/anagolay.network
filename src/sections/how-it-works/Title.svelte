4e8655c435d949b5f07cc4557d71a1348d0cebce
Leonardo Monay <leonardomonay@gmail.com>
2022-06-08 14:01:27 -0300

    change and fit HIW image.

--- src/sections/how-it-works/Title.svelte
+++ src/sections/how-it-works/Title.svelte
@@ -4,7 +4,7 @@
 
 <Section backgroundClass="bg-ligtTopRight md:bg-none from-aquaTeal-300/50 to-transparent">
 	<div class="flex">
-		<div class="w-5/6 mx-auto md:w-full pt-28">
+		<div class="w-5/6 mx-auto md:w-full pt-20 pb-8">
 			<div>
 				<h1 class="tracking-wider text-center font-extrabold text-3xl md:text-5xl md:text-left min-w-fit">
 					<p class="md:leading-[4rem] min-w-fit">How does it work?</p>
@@ -29,7 +29,7 @@
 			/>
 		</div>
 
-		<div class="hidden md:flex -ml-[100%] w-full mt-40 lg:mt-20 xl:-mt-4">
+		<div class="hidden md:flex -ml-[100%] w-full mt-44 lg:mt-24 2xl:mt-4">
 			<img
 				class="relative w-full mx-auto xl:w-5/6"
 				src="/how_it_works_main_workflow.png"
@@ -45,7 +45,7 @@
 		</div>
 	</div>
 </Section>
-<div class="w-5/6 sm:w-[33rem] text-lg md:text-4xl text-center mx-auto max-w-full pt-8 md:py-32">
+<div class="w-5/6 sm:w-[33rem] text-lg md:text-4xl text-center mx-auto max-w-full pt-8 md:pt-44 md:pb-32">
 	<p class="md:leading-[3.25rem]">
 		Anagolay allows asset creators to establish how digital assets are used and owned.
 	</p>
