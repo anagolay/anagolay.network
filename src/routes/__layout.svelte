220577a4f33ad7fabc3d1819d7f2f7520e364e37
Leonardo Monay <leonardomonay@gmail.com>
2022-06-13 09:28:22 -0300

    set background color in app.css

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,14 +3,6 @@
 	import NavBar from '$src/components/NavBar.svelte';
 	import TopButton from '$src/components/TopButton.svelte';
 	import Footer from '$src/components/Footer.svelte';
-	import { onMount } from 'svelte';
-	// snap-y snap-proximity snap-always
-
-	onMount(() => {
-		const bgColor = 'rgb(5, 34, 51)';
-		document.body.style.backgroundColor = bgColor;
-		document.documentElement.style.backgroundColor = bgColor;
-	});
 </script>
 
 <NavBar />
