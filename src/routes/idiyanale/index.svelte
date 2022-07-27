a1c7de5452425d9c6a9d27dc76ea24256b7624f3
Leonardo Monay <leonardomonay@gmail.com>
2022-07-27 16:01:20 -0300

    add SEO and twitter card.

--- src/routes/idiyanale/index.svelte
+++ src/routes/idiyanale/index.svelte
@@ -2,13 +2,28 @@
 	import type { FeatureData } from '$src/types';
 
 	import TitleSection from '$src/sections/idiyanale/Title.svelte';
-	// import AfterLaunch from '$src/sections/idiyanale/AfterLaunch.svelte';
 	import Description from '$src/sections/idiyanale/Description.svelte';
 	import Process from '$src/sections/idiyanale/Process.svelte';
+	import SvelteSeo from 'svelte-seo';
 
 	export let processCards: string[];
 	export let cardsContent: FeatureData[];
 </script>
+
+<SvelteSeo
+	title="Idiyanale Network - Pioneer MainNet for Anagolay Framework"
+	description="A playground for developers to try and test both new and existing features of Anagolay framework in an unrefined, unpredictable but realistic on-chain environment. Workflows & Operations stored on Idiyanale can be used to verify authorship, protect and manage copyright & ownership."
+	keywords="idiyanale, anagolay, blockchain, workflow, transparent process, substrate, 
+	copyright statements, ownership statements, web3 rights management, pioneer network"
+	twitter={{
+		site: '@AnagolayNet',
+		title: 'Idiyanale Network - Pioneer MainNet for Anagolay Framework',
+		description:
+			'A playground for developers to try and test both new and existing features of Anagolay framework in an unrefined, unpredictable but realistic on-chain environment. Workflows & Operations stored on Idiyanale can be used to verify authorship, protect and manage copyright & ownership.',
+		image: '/idiyanale_twittercard.png',
+		imageAlt: 'Blue background with Idiyanale white brand logo',
+	}}
+/>
 
 <div>
 	<TitleSection />
