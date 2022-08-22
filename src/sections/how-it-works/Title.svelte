ddb14a982096b0b9917c7d932862ef8c792ade62
Daniel Maricic <daniel@woss.io>
2022-08-22 14:47:36 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/sections/how-it-works/Title.svelte
+++ src/sections/how-it-works/Title.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { whitePaperUrl } from './../../routes';
+	import { whitePaperUrl } from '../../routes';
 	import Section from '$src/components/Section.svelte';
 	import SectionButton from '$src/components/SectionButton.svelte';
 	import TrustCard from '$src/components/TrustCard.svelte';
