9c7a3f711600078d7f26145c78dcceedcf61ffe6
Leonardo Monay <leonardomonay@gmail.com>
2023-02-17 12:07:20 -0300

    fix svelte-seo import

--- src/routes/idiyanale/phases/+page.svelte
+++ src/routes/idiyanale/phases/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Phases from '$src/sections/idiyanale/phases/Phases.svelte';
 	import Title from '$src/sections/idiyanale/phases/Title.svelte';
-	import SvelteSeo from 'svelte-seo/types/SvelteSeo';
+	import SvelteSeo from 'svelte-seo';
 </script>
 
 <SvelteSeo
